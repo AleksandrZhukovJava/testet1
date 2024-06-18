1c807a28fc7ec88ffe92f94374df1047ba92b37e
Александр Жуков <a.zhukov@uzum.com>
2024-06-18 19:52:55 +0200

    homewrok done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
+        System.out.println("Привет мир!");
     }
 }
